6154445f326ba0b156150474cc15b720c85db085
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-08-05 20:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your code here.
+const items = document.querySelector('.items');
+let isDragging = false;
+let startPosX = 0;
+let scrollLeft = 0;
+
+items.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startPosX = e.pageX - items.offsetLeft;
+  scrollLeft = items.scrollLeft;
+});
+
+items.addEventListener('mouseleave', () => {
+  isDragging = false;
+});
+
+items.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+items.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX - items.offsetLeft;
+  const walk = (x - startPosX) * 3; // Adjust the sliding speed
+  items.scrollLeft = scrollLeft - walk;
+});
